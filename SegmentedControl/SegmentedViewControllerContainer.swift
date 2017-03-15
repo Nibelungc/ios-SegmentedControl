b2948d55da2408aef749052dc932161fbcfb6d48
Николай Кагала <nikolay.kagala@e-legion.com>
2017-03-15 20:00:59 +0300

    Create SegmentedViewControllerContainer

---
+++ SegmentedControl/SegmentedViewControllerContainer.swift
@@ -0,0 +1,112 @@
+//
+//  SegmentedViewControllerContainer.swift
+//  SegmentedControl
+//
+//  Created by Николай Кагала on 15/03/2017.
+//  Copyright © 2017 Николай Кагала. All rights reserved.
+//
+
+import UIKit
+
+
+protocol SegmentedViewControllerContainerDataSource: class {
+    func numberOfControllers(in container: SegmentedViewControllerContainer) -> Int
+    func initialController(in container: SegmentedViewControllerContainer) -> UIViewController
+    func controller(in container: SegmentedViewControllerContainer, atIndex index: Int) -> UIViewController?
+}
+
+@objc protocol SegmentedViewControllerContainerDelegate: class {
+
+}
+
+class SegmentedViewControllerContainer: UIViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    private var pageController: UIPageViewController!
+    private var indicies = [UIViewController: Int]()
+    
+    weak var dataSource: SegmentedViewControllerContainerDataSource? {
+        didSet { reloadData() }
+    }
+    var currentControllerIndex: Int = 0
+    
+    //MARK: - Lifecycle
+    
+    init(parentViewController: UIViewController, frame: CGRect) {
+        super.init(nibName: nil, bundle: nil)
+        addChildViewController(self, toParent: parentViewController, with: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    private func setupUI() {
+        pageController = UIPageViewController(transitionStyle: .scroll,
+                                              navigationOrientation: .horizontal)
+        guard let initialVC = dataSource?.initialController(in: self) else { return }
+        setupPageController(pageController, initialViewController: initialVC)
+        addChildViewController(pageController, toParent: self, with: view.bounds)
+    }
+    
+    private func resetUI() {
+        
+    }
+    
+    //MARK: - Public
+
+    func reloadData() {
+        resetUI()
+        setupUI()
+    }
+    
+    //MARK: - Private
+    
+    private func setupPageController(_ controller: UIPageViewController, initialViewController initial: UIViewController) {
+        controller.dataSource = self
+        controller.delegate = self
+        controller.view.frame = view.frame
+        controller.setViewControllers([initial], direction: .forward, animated: false)
+    }
+    
+    private func addChildViewController(_ childController: UIViewController,
+                                        toParent parent: UIViewController,
+                                        with frame: CGRect) {
+        parent.addChildViewController(childController)
+        childController.view.frame = frame
+        parent.view.addSubview(childController.view)
+        childController.didMove(toParentViewController: parent)
+    }
+    
+    //MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return viewControlerCalculatingIndex(calculation: { $0 - $1 })
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return viewControlerCalculatingIndex(calculation: { $0 + $1 })
+    }
+    
+    private func viewControlerCalculatingIndex(calculation: (Int, Int) -> Int) -> UIViewController? {
+        guard let dataSource = dataSource else { return nil }
+        let newIndex = calculation(currentControllerIndex, 1)
+        guard (0..<dataSource.numberOfControllers(in: self)).contains(newIndex) else { return nil }
+        let controller = dataSource.controller(in: self, atIndex: newIndex)
+        if controller != nil { indicies[controller!] = newIndex }
+        return controller
+    }
+    
+    //MARK: - UIPageViewControllerDelegate
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            didFinishAnimating finished: Bool,
+                            previousViewControllers: [UIViewController],
+                            transitionCompleted completed: Bool) {
+        guard let controller = pageViewController.viewControllers?.last else { return }
+        if let index = indicies[controller] {
+            currentControllerIndex = index
+        }
+    }
+}
--- SegmentedControl/ViewController.swift
+++ SegmentedControl/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, SegmentedControlDelegate,  SegmentedControlDataSource {
+class ViewController: UIViewController, SegmentedControlDelegate,  SegmentedControlDataSource, SegmentedViewControllerContainerDataSource {
     
     var data: Pack = .quotes
+    var segmentedViewController: SegmentedViewControllerContainer!
     
     enum Pack {
         case quotes
@@ -27,11 +28,35 @@ class ViewController: UIViewController, SegmentedControlDelegate,  SegmentedCont
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var frame = view.bounds
+        frame.origin.y = segmentedControl.frame.maxY
+        frame.size.height -= segmentedControl.frame.maxY - view.frame.minY
+        segmentedViewController = SegmentedViewControllerContainer(parentViewController: self, frame: frame)
+        segmentedViewController.dataSource = self
+        segmentedViewController.view.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
+        
         automaticallyAdjustsScrollViewInsets = false
         segmentedControl.dataSource = self
         segmentedControl.delegate = self
         segmentedControl.backgroundColor = .groupTableViewBackground
         segmentedControl.reloadData()
+        
+        
+    }
+    
+    //MARK: - SegmentedViewControllerContainerDataSource
+    
+    func numberOfControllers(in container: SegmentedViewControllerContainer) -> Int {
+        return data.items.count
+    }
+    
+    func initialController(in container: SegmentedViewControllerContainer) -> UIViewController {
+        return viewController(forItemWithTitle: data.items.first!)
+    }
+    
+    func controller(in container: SegmentedViewControllerContainer, atIndex index: Int) -> UIViewController? {
+        return viewController(forItemWithTitle: data.items[index])
     }
     
     //MARK: - SegmentedControlDataSource
@@ -44,7 +69,7 @@ class ViewController: UIViewController, SegmentedControlDelegate,  SegmentedCont
         return data.items[index]
     }
     
-    //MARK: - delegate
+    //MARK: - SegmentedControlDelegate
     
     func segmentedControl(_ segmentedControl: SegmentedControl, didSelectItemAt index: Int) {
         
@@ -54,6 +79,20 @@ class ViewController: UIViewController, SegmentedControlDelegate,  SegmentedCont
         data = data == .quotes ? .briefcase : .quotes
         segmentedControl.reloadData()
         segmentedControl.selectedSegmentIndex = 0
+    }
+    
+    //MARK: - Private
+    
+    private func viewController(forItemWithTitle title: String) -> UIViewController {
+        let controller = UIViewController()
+        controller.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        let label = UILabel()
+        label.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin, .flexibleWidth, .flexibleHeight]
+        label.text = title
+        label.sizeToFit()
+        label.center = controller.view.center
+        controller.view.addSubview(label)
+        return controller
     }
 }
 
